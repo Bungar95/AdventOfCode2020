847b8544b62d86f3cfcbb80284b7017d705f1da4
Borna Ungar <borna.ungar@gmail.com>
2020-12-10 22:34:36 +0100

    day6 puzzle 2

--- src/main/kotlin/dailyChallenges/day6/day6.kt
+++ src/main/kotlin/dailyChallenges/day6/day6.kt
@@ -2,25 +2,43 @@ package dailyChallenges.day6
 
 import dailyChallenges.loadTxtAsString
 
-var answerPot: Int = 0
+var p1AnswerPot: Int = 0
+var p2AnswerPot: Int = 0
 
 fun day6() {
     val array = loadTxtAsString("src/main/kotlin/dailyChallenges/day6/day6.txt")
-    var groupArray = mutableListOf<Char>()
+    var uniqueCharArray = mutableListOf<Char>()
+    var rowGroupArray = mutableListOf<String>()
 
     for(row in array) {
         if(row.isNotBlank()){
+            rowGroupArray.add(row)
             for (i in row) {
-                if (!groupArray.contains(i)) {
+                if (!uniqueCharArray.contains(i)) {
                     //print(i)
-                    groupArray.add(i)
+                    uniqueCharArray.add(i)
                 }
             }
+
         }else{
-            println("$groupArray, ${groupArray.size}")
-            answerPot += groupArray.size
-            groupArray.clear()
+            //println("$uniqueCharArray, ${uniqueCharArray.size}")
+            p1AnswerPot += uniqueCharArray.size
+            checkDay6Puzzle2(uniqueCharArray, rowGroupArray)
+            uniqueCharArray.clear()
+            rowGroupArray.clear()
         }
     }
-    println("Total: $answerPot")
+    println("Puzzle 1 total: $p1AnswerPot")
+    println("Puzzle 2 total: $p2AnswerPot")
+}
+
+fun checkDay6Puzzle2(uniqueCharArray: MutableList<Char>, rowGroupArray: MutableList<String>) {
+
+    for(i in uniqueCharArray){
+        var checkChar = 0
+        for(row in rowGroupArray){
+            if(row.contains(i)) checkChar++
+        }
+        if(checkChar == rowGroupArray.size) p2AnswerPot++
+    }
 }

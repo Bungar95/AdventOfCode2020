5f466657c5a45be6bcc190e9efce34a8d93cebff
Borna Ungar <borna.ungar@gmail.com>
2020-12-17 00:42:42 +0100

    day9 puzzle2

--- src/main/kotlin/dailyChallenges/day9/Day9.kt
+++ src/main/kotlin/dailyChallenges/day9/Day9.kt
@@ -7,25 +7,58 @@ class Day9 {
     private val numbers = mutableListOf<Long>()
     private var previous25 = listOf<Long>()
 
-    fun day9(){
+    fun day9() {
         val array = loadTxtAsString("src/main/kotlin/dailyChallenges/day9/day9.txt")
-        for(i in array){
+        for (i in array) {
             numbers.add(i.toLong())
         }
         val start = 25
         checkNumber(start)
-
     }
 
-    private fun checkNumber(l: Int) {
-        var currentNumber = l
+    private fun checkNumber(indexNum: Int) {
+        var currentNumber = indexNum
         var checkState = 0
         previous25 = numbers.slice((currentNumber - 25) until currentNumber)
         for (i in previous25) {
-            if (previous25.contains(numbers[currentNumber]-i)) checkState++
+            if (previous25.contains(numbers[currentNumber] - i)) checkState++
         }
-        if(checkState == 0) println(numbers[l])
-        else checkNumber(++currentNumber)
+        if (checkState == 0) {
+            println("Puzzle 1: ${numbers[indexNum]}")
+            println("Puzzle 2: ${startPuzzle2(numbers[indexNum])}")
+        } else checkNumber(++currentNumber)
+    }
+
+    private fun startPuzzle2(indexNum: Long): Long {
+
+        var i = 0
+        var j = 1
+        var sum = numbers[i] + numbers[j]
+
+        fun increaseJ() {
+            j++
+            sum += numbers[j]
+        }
+
+        while (j < numbers.size) {
+            when {
+                sum < indexNum -> increaseJ()
+                sum > indexNum -> {
+                    sum -= numbers[i]
+                    i++
+                    if (i == j) increaseJ()
+                }
+                else -> {
+                    val range = i..j
+                    val min = numbers.slice(range).minOrNull()!!
+                    val max = numbers.slice(range).maxOrNull()!!
+                    return max+min
+                }
+            }
+        }
+        return 0
     }
 
 }
+
+

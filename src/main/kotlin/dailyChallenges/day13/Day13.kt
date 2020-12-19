dce821983244a4267aca799bccf082539173d538
Borna Ungar <borna.ungar@gmail.com>
2020-12-20 00:31:41 +0100

    day13 puzzle 1

--- src/main/kotlin/dailyChallenges/day13/Day13.kt
+++ src/main/kotlin/dailyChallenges/day13/Day13.kt
@@ -3,14 +3,34 @@ package dailyChallenges.day13
 import dailyChallenges.loadTxtAsString
 
 class Day13 {
+    var array = loadTxtAsString("src/main/kotlin/dailyChallenges/day13/day13.txt")
+    private val earliestDepart = array[0].toLong()
+    var busIds = emptyList<Long>().toMutableList()
+    var earliestBus: Long = earliestDepart
+    var earliestBusId: Long = 0
 
     fun day13(){
-        var busIds = emptyList<Int>().toMutableList()
-        var array = loadTxtAsString("src/main/kotlin/dailyChallenges/day13/day13.txt")
 
-        for(i in array[1].split(",")) if(isNumber(i)) busIds.add(i.toInt())
-        println(busIds)
+        for(i in array[1].split(",")) if(isNumber(i)) busIds.add(i.toLong())
+        //println(busIds)
+        for(i in busIds) checkTime(i)
+        println("Puzzle 1: Earliest bus is $earliestBusId at $earliestBus, result: ${(earliestBus-earliestDepart)*earliestBusId}")
+    }
 
+    private fun checkTime(i: Long) {
+        var time= i
+        while(time < earliestDepart)
+            time += i
+        checkIfEarliestBus(time, i)
+        //println("$i bus can come at $time")
+    }
+
+    private fun checkIfEarliestBus(time: Long, i: Long) {
+        if(earliestBus == earliestDepart) earliestBus = time
+        else if(time < earliestBus) {
+            earliestBus = time
+            earliestBusId = i
+        }
     }
 
     private fun isNumber(string: String): Boolean {
